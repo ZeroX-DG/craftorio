9fc0c850c3d8e6b4c09755303d26523d5834d592
ZeroX-DG <viethungax@gmail.com>
2024-07-04 19:57:56 +1200

    (experiment): instanced rendering threejs

--- src/components/MainGame.tsx
+++ src/components/MainGame.tsx
@@ -11,13 +11,16 @@ export const MainGame = () => {
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
 
+        scene.add(new THREE.DirectionalLight(0xf7d67c, 5));
+        scene.add(new THREE.AmbientLight(0x404040));
+
         const controls = new OrbitControls(camera, renderer.domElement);
         controls.target.set(0, 0, 0);
         controls.update();
 
         scene.add(new THREE.GridHelper(100, 10));
 
-        const worldSize = { width: 5, height: 5, depth: 5 };
+        const worldSize = { width: 10, height: 10, depth: 10 };
         const textureLoader = new THREE.TextureLoader();
         textureLoader.setPath('textures/');
 
@@ -25,26 +28,32 @@ export const MainGame = () => {
         const dirtTexture = textureLoader.load('dirt.webp');
         const grassTopTexture = textureLoader.load('grass_top.webp');
 
-        const grassSideMaterial = new THREE.MeshBasicMaterial({ map: grassSideTexture });
-        const dirtMaterial = new THREE.MeshBasicMaterial({ map: dirtTexture });
-        const grassTopMaterial = new THREE.MeshBasicMaterial({ map: grassTopTexture });
+        const grassSideMaterial = new THREE.MeshStandardMaterial({ map: grassSideTexture });
+        const dirtMaterial = new THREE.MeshStandardMaterial({ map: dirtTexture });
+        const grassTopMaterial = new THREE.MeshStandardMaterial({ map: grassTopTexture });
         const blockGeometry = new THREE.BoxGeometry();
+
+        const blockMaterial = [
+            grassSideMaterial, // side
+            grassSideMaterial, // side
+            grassTopMaterial, // top
+            dirtMaterial, // bottom
+            grassSideMaterial, // side
+            grassSideMaterial, // side
+        ];
+        const blockMesh = new THREE.InstancedMesh(blockGeometry, blockMaterial, worldSize.width * worldSize.height * worldSize.depth);
+        scene.add(blockMesh);
+        const blockMatrix = new THREE.Object3D();
 
         for (let x = 0; x < worldSize.width; x++) {
             for (let y = 0; y < worldSize.height; y++) {
                 for (let z = 0; z < worldSize.depth; z++) {
-                    const block = new THREE.Mesh(blockGeometry, [
-                        grassSideMaterial, // side
-                        grassSideMaterial, // side
-                        grassTopMaterial, // top
-                        dirtMaterial, // bottom
-                        grassSideMaterial, // side
-                        grassSideMaterial, // side
-                    ]);
-                    block.position.x = x;
-                    block.position.y = -y;
-                    block.position.z = z;
-                    scene.add(block);
+                    blockMatrix.position.x = x;
+                    blockMatrix.position.y = -y;
+                    blockMatrix.position.z = z;
+                    const index = x * (worldSize.height * worldSize.depth) + y * worldSize.depth + z;
+                    blockMatrix.updateMatrix();
+                    blockMesh.setMatrixAt(index, blockMatrix.matrix);
                 }
             }
         }
