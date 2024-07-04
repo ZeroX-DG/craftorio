ac2ef7c933be09d8b26d619b645d3bd1da0455d5
ZeroX-DG <viethungax@gmail.com>
2024-07-04 21:50:51 +1200

    (fix): fixed public path repo when deploying to prod

--- farm.config.ts
+++ farm.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from '@farmfe/core';
-import solid from 'vite-plugin-solid';
+import { defineConfig } from "@farmfe/core";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
+  compilation: {
+    output: {
+      publicPath: process.env.NODE_ENV === "production" ? "/craftorio/" : "/",
+    },
+  },
   vitePlugins: [
     () => ({
       vitePlugin: solid(),
-      filters: ['\\.tsx$', '\\.jsx$']
-    })
-  ]
+      filters: ["\\.tsx$", "\\.jsx$"],
+    }),
+  ],
 });
